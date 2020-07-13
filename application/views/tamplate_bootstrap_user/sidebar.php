428d82f8db621dddb80fff8e0b7913cafb771cd5
nangjipang141098 <nangjipang@gmail.com>
2020-07-13 10:11:45 +0700

    update

--- application/views/tamplate_bootstrap_user/sidebar.php
+++ application/views/tamplate_bootstrap_user/sidebar.php
@@ -10,29 +10,29 @@
     </a>
       <div class="dropdown-menu" aria-labelledby="pagesDropdown">
         <h6 class="dropdown-header">Action</h6>
-        <a class="dropdown-item" href="<?php echo base_url() ?>hrd/dashboard_hrd">Data Seluruh Karyawan</a>
-        <a class="dropdown-item" href="<?php echo base_url() ?>hrd/dashboard_hrd/add">Tambah Karyawan</a>
+        <a class="dropdown-item" href="<?php echo base_url() ?>user/dashboard_hrd">Data Seluruh Karyawan</a>
+        <a class="dropdown-item" href="<?php echo base_url() ?>user/dashboard_hrd/add">Tambah Karyawan</a>
       </div>
   </li>
   <li class="nav-item active">
-    <a class="nav-link" href="<?php echo base_url() ?>hrd/kehadiran">
+    <a class="nav-link" href="<?php echo base_url() ?>user/kehadiran">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Kehadiran Karyawan</span>
     </a>
   </li>
   
   <li class="nav-item">
-    <a class="nav-link" href="<?php echo base_url() ?>hrd/uangtransport">
+    <a class="nav-link" href="<?php echo base_url() ?>user/uangtransport">
       <i class="fas fa-fw fa-chart-area"></i>
       <span>Uang Transport</span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="<?php echo base_url() ?>hrd/perjalanandinas">
+    <a class="nav-link" href="<?php echo base_url() ?>user/perjalanandinas">
       <i class="fas fa-fw fa-chart-area"></i>
       <span>Perjalanan Dinas</span></a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="<?php echo base_url() ?>hrd/medicalreimbust">
+    <a class="nav-link" href="<?php echo base_url() ?>user/medicalreimbust">
       <i class="fas fa-fw fa-chart-area"></i>
       <span>Medical Reimburstment</span></a>
   </li>
