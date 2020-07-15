93b96cd70db0f0932c901452c8fbba6709e56111
nangjipang141098 <nangjipang@gmail.com>
2020-07-15 22:31:58 +0700

    update

--- application/controllers/manajemen/profile.php
+++ application/controllers/manajemen/profile.php
@@ -4,8 +4,7 @@ class Profile extends CI_Controller{
 
 	public function __construct(){
 		parent::__construct();
-		
-		if($this->session->userdata('role_id') != '2'){
+		if($this->session->userdata('role_id') != '3'){
 			$this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
 			Anda Belum Login HRD!
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -18,100 +17,25 @@ class Profile extends CI_Controller{
 		$this->load->library('session');
 		$this->load->library('upload');
 		$this->load->helper('url');
-		if( ($this->session->userdata('id_karyawan') == null) && ($this->session->userdata('role_id') != 2) ){
+		if( ($this->session->userdata('id_karyawan') == null) && ($this->session->userdata('role_id') != 3) ){
 			redirect('login/login');
 		}
-		
     }
 
     public function index()
 	{
 		$data = array('title' => 'Data Karyawan',
-					  'content' => 'hrd/profile/profile'
+					  'content' => 'user/profile/profile'
                      );
                      
-		$this->load->view('tamplate_bootstrap_hrd/wrapper', $data, FALSE);
-	}
-
-	public function delete()
-	{
-		$data = array('title' => 'Data Karyawan',
-					  'content' => 'hrd/karyawan/list'
-					 );
-					
-					$id = $this->input->get('id');
-					$where = array('id_karyawan' => $id);
-					$this->karyawan->delete($where);
-					$this->load->view('tamplate_bootstrap_hrd/wrapper', $data, FALSE);	
-	}
-
-	public function add(){
-		$data = array('title' => 'Add Karyawan',
-					  'content' => 'hrd/karyawan/add'
-		);
-		$this->load->view('tamplate_bootstrap_user/wrapper', $data, FALSE);
-		$config['upload_path'] = './assets/images/'; //path folder
-	    $config['allowed_types'] = 'gif|jpg|png|doc|docx|pdf|xls|xlsx|ppt|ppt|zip|rar'; //type yang dapat diakses bisa anda sesuaikan
-	    $config['encrypt_name'] = TRUE; //Enkripsi nama yang terupload
-		$config['max_size']             = 7000;
-		$config['max_width']            = 0;
-		$config['max_height']           = 0;
-		$this->upload->initialize($config);
-
-		if(!empty($_FILES['filefoto']['name'])){
-			if ($this->upload->do_upload('filefoto')){ //nama fiel yg ada diview
-				$gbr = $this->upload->data();
-	            //Compress Image
-	            $config['image_library']='gd2';
-	            $config['source_image']='./assets/images/karyawan/'.$gbr['file_name'];
-	            $config['new_image']= './assets/images/karyawan/'.$gbr['file_name'];
-	            $this->load->library('image_lib', $config);
-	            $this->image_lib->resize();
-				$id_karyawan		= $this->input->post('id_karyawan');
-				$nama_karyawan		= $this->input->post('nama_karyawan');
-				$jenis_kelamin		= $this->input->post('jenis_kelamin');
-				$kode_bagian		= $this->input->post('kode_bagian');
-				$alamat				= $this->input->post('alamat');
-				$nomor_telepon		= $this->input->post('nomor_telepon');
-				$email				= $this->input->post('email');
-				$tanggal_lahir		= $this->input->post('tanggal_lahir');
-				$password			= $this->input->post('password');
-				$role_id			= 3;
-				$foto 				= $gbr['file_name'];
-				$cek_employe 		= $this->karyawan->check_employe($id_karyawan);
-				$cek_data_employe 	= $cek_employe->num_rows();
-					if($cek_data_employe > 0){
-						foreach($cek_employe->result_array() as $row){
-							echo "<script>alert('ID KARYAWAN".$row['id_karyawan']." sudah didaftarkan ')</script>";
-						}
-					}
-					else{
-						$data_user = array(
-							'id_karyawan'		=> $id_karyawan,
-							'nama_karyawan'		=> $nama_karyawan,
-							'jenis_kelamin'		=> $jenis_kelamin,
-							'kode_bagian'		=> $kode_bagian,
-							'alamat'			=> $alamat,
-							'nomor_telepon'		=> $nomor_telepon,
-							'email'				=> $email,
-							'tanggal_lahir'		=> $tanggal_lahir,
-							'password'			=> $password,
-							'role_id'			=> 3,
-							'foto'				=> $foto
-						);
-						$this->karyawan->add_employe($data_user);
-						echo "<script>alert('UPLOAD DATA BERHASIL')</script>";
-					}
-			}
-		}
-		
+					 $this->load->view('tamplate_bootstrap_user/wrapper', $data, FALSE);
 	}
 	
 	public function edit(){
 		$data = array('title' => 'Add Karyawan',
-					  'content' => 'hrd/profile/profile'
+					  'content' => 'user/profile/profile'
 		);
-		$this->load->view('tamplate_bootstrap_hrd/wrapper', $data, FALSE);
+		$this->load->view('tamplate_bootstrap_user/wrapper', $data, FALSE);
 		$id = $this->input->get('id');
 		
 
