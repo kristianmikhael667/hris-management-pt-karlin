045447fc849f20c228b07a90fe6828e15973060f
kristianmikhael667 <kristianmikhael667@gmail.com>
2020-08-05 18:41:16 +0700

    waktu adalah uang

--- application/views/user/absensi/js_face_recognition.php
+++ application/views/user/absensi/js_face_recognition.php
@@ -29,7 +29,58 @@
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
+                        <div class="modal-body">
+                        <style>
+                                h1,h2,p,a{
+                                    font-family: sans-serif;
+                                    font-weight: normal;
+                                }
+                            
+                                .jam-digital-malasngoding {
+                                    overflow: hidden;
+                                    width: 100px;
+                                    margin-left: 10px;
+                                    border: 5px solid #efefef;
+                                }
+                                .kotak{
+                                    float: left;
+                                    width: 30px;
+                                    height: 45px;
+                                    background-color: #00FF00	;
+                                }
+                                .jam-digital-malasngoding p {
+                                    color: #fff;
+                                    font-size: 20px;
+                                    text-align: center;
+                                    margin-top: 10px;
+                                }                       
+                            
+                            </style>
+                            
+                            <div class="jam-digital-malasngoding">
+                                <div class="kotak">
+                                    <p id="jam"> : </p>
+                                </div>
+                                <div class="kotak">
+                                    <p id="menit"> : </p>
+                                </div>
+                                <div class="kotak">
+                                    <p id="detik"> : </p>
+                                </div>
+                            </div>
 
+                            <script>
+                                window.setTimeout("waktu()", 1000);
+                            
+                                function waktu() {
+                                    var waktu = new Date();
+                                    setTimeout("waktu()", 1000);
+                                    document.getElementById("jam").innerHTML = waktu.getHours();
+                                    document.getElementById("menit").innerHTML = waktu.getMinutes();
+                                    document.getElementById("detik").innerHTML = waktu.getSeconds();
+                                }
+                            </script>
+                        </div>
                         <div class="modal-body">
 
                         <?php $session_value = $this->session->userdata('id_karyawan');
