65c66fb53e782595941c12b1b514622c335b6a11
nangjipang141098 <nangjipang@gmail.com>
2020-08-19 14:43:02 +0700

    update tabel kehadiran di karyawan

--- application/views/user/kehadiran/list.php
+++ application/views/user/kehadiran/list.php
@@ -96,54 +96,6 @@
 
 
   <!-- Pie Chart -->
-<<<<<<< HEAD
-<div class="col-xl-4 col-lg-5">
-  <div class="card shadow mb-4">
-    <!-- Card Header - Dropdown -->
-    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-      <h6 class="m-0 font-weight-bold text-primary">Record Kehadiran</h6>
-        <div class="dropdown no-arrow">
-          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-          </a>       
-        </div>
-      </div>
-      <!-- Card Body -->
-    <div class="card-body">
-      <div class="chart-pie pt-4 pb-2">
-        <div class="card mb-3">
-          <div class="card-header">
-            <i class="fas fa-table"></i>
-              Tabel Kehadiran
-          </div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                  <tr>
-                    <th>Jumlah Hadir</th>
-                    <th>Jumlah Cuti</th>
-                    <th>Jumlah Izin</th>
-                    <th>Jumlah Sakit</th>
-                  </tr>
-                </thead>
-              <tbody>
-                <?php $id_karyawan = $this->session->userdata('id_karyawan');
-                  echo $id_karyawan;
-                  $cek_query=$this->kehadiran->kehadiran($id_karyawan); 
-                  foreach ($cek_query->result_array() as $row)
-                  {          
-                ?>
-                <tr>
-                  <td><?php echo $row['jumlah_hadir'] ; ?></td>
-                  <td><?php echo $row['jumlah_cuti'] ; ?> </td>
-                  <td><?php echo $row['jumlah_izin'] ; ?> </td>
-                  <td><?php echo $row['jumlah_sakit'] ; ?> </td>
-                </tr>
-                <?php } ?>    
-              </tbody>
-              </table>
-=======
   <div class="col-xl-4 col-lg-5">
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
@@ -235,15 +187,8 @@
                   </div>
                 </div>
               </div>
->>>>>>> ee50ce5286cf37123f002e55c686d783aaadbe2b
             </div>
           </div>
-        </div> 
-      </div>
-    </div>
-  </div>
-</div>
-</div>
 
 <!-- Content Row -->
 <div class="row">
