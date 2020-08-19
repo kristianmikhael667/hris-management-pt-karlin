35df84edd5343a328059147e3bb6c2c7f2d4370a
kristianmikhael667 <kristianmikhael667@gmail.com>
2020-08-19 13:22:02 +0700

    add font awesome

--- application/views/tamplate_bootstrap_user/sidebar.php
+++ application/views/tamplate_bootstrap_user/sidebar.php
@@ -5,21 +5,21 @@
 <ul class="sidebar navbar-nav">
   <li class="nav-item">
     <a class="nav-link" href="<?php echo base_url() ?>user/dashboard_user">
-      <i class="fas fa-fw fa-tachometer-alt"></i>
+    <i class="fa fa-home" aria-hidden="true"></i>
       <span>Dashboard User</span>
     </a>
   </li>
 
   <li class="nav-item">
     <a class="nav-link" href="<?php echo base_url() ?>user/kontak">
-      <i class="fas fa-fw fa-tachometer-alt"></i>
+    <i class="fa fa-address-book" aria-hidden="true"></i>
       <span>Kontak Karyawan</span>
     </a>
   </li>
 
   <li class="nav-item">
     <a class="nav-link" href="<?php echo base_url() ?>user/absen">
-      <i class="fas fa-fw fa-tachometer-alt"></i>
+    <i class="fa fa-camera" aria-hidden="true"></i>
       <span>Absen Karyawan</span>
     </a>
   </li>
@@ -27,21 +27,22 @@
 
   <li class="nav-item">
     <a class="nav-link" href="<?php echo base_url() ?>user/medicalreimbust">
-      <i class="fas fa-fw fa-chart-area"></i>
+    <i class="fa fa-plus-square" aria-hidden="true"></i>
+
       <span>Medical Reimbust</span></a>
   </li>
   
   
   
-    <li class="nav-item">
+  <li class="nav-item">
     <a class="nav-link" href="<?php echo base_url() ?>user/formcuti">
-      <i class="fas fa-fw fa-chart-area"></i>
+    <i class="fa fa-edit" aria-hidden="true"></i>
       <span>Form Cuti</span></a>
   </li>
 
   <li class="nav-item">
     <a class="nav-link" href="<?php echo base_url() ?>user/formdinas">
-      <i class="fas fa-fw fa-chart-area"></i>
+      <i class="fas fa-edit"></i>
       <span>Form Perjalanan Dinas</span></a>
   </li>
 
@@ -49,7 +50,7 @@
 
   <li class="nav-item">
     <a class="nav-link" href="<?php echo base_url() ?>user/formpurchase">
-      <i class="fas fa-fw fa-chart-area"></i>
+      <i class="fas fa-edit"></i>
       <span>Forum Purchase Request</span></a>
   </li>
 
