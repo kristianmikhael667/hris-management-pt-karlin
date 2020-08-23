9c77a54c1837efec7d29d6c4c011ffde8f7972a0
nangjipang141098 <nangjipang@gmail.com>
2020-08-23 15:29:53 +0700

    edit data medical at hrd

--- application/views/hrd/medical_reimburstment/list.php
+++ application/views/hrd/medical_reimburstment/list.php
@@ -1,11 +1,7 @@
-
 <div id="content-wrapper">
 
       <div class="container-fluid">
-
       
-      <div id="content-wrapper">
-      <div class="container-fluid">
         <!-- DataTables Example -->
         <div class="card mb-3">
           <div class="card-header">
@@ -39,7 +35,7 @@
                   {       
                 ?>
                   <tr class="text-center">
-                    <td><?php echo $row['id_karyawan'] ; ?></td>
+                  <td><?php echo $row['id_karyawan'] ; ?></td>
                     <td><?php echo $row['nama_karyawan'] ; ?></td>
                     <td><?php echo $row['klaim_id'] ; ?></td>
                     <td><?php echo $row['tanggal_pengajuan'] ; ?></td>
@@ -49,8 +45,16 @@
                     <td><?php echo $row['jumlah_disetujui'] ; ?></td>
                     <td><img style="width: 100px;" src="<?php echo base_url().'assets/images/'.$row['struck'];?>"></td>
                     <td><?php echo $row['ket'] ; ?></td>
-                    <td><a href="<?php echo base_url('hrd/medicalreimbust/delete?id=') . $row['id_karyawan']; ?>" class="btn btn-outline-danger"> Hapus </a>
-                    <a href="<?php echo base_url('hrd/medicalreimbust/edit?id=') . $row['id_karyawan']; ?>" class="btn btn-outline-success"> Edit </a>  </td>
+                    <td>
+                    <button class="btn btn-sm btn-primary mb-2" data-toggle="modal" data-target="#view" name=""><i class="fa fa-eye"></i> View</button>
+                  
+                      <?php
+                        $cek_query=$this->medical->list(); 
+                        foreach ($cek_query->result_array() as $row)
+                        { ?>
+                          <a class="btn btn-sm btn-danger mb-2" href="<?php echo base_url('hrd/medicalreimbust/delete?id=') . $row['id_karyawan']; ?>"><i class="fa fa-trash"></i> Delete</a>
+                      <?php } ?>
+                        <button class="btn btn-sm btn-success mb-2" data-toggle="modal" data-target="#edit" name=""><i class="fa fa-magic"></i> Edit</button></td>
                   </tr>
 
                   <?php } ?>
@@ -60,9 +64,72 @@
             </div>
           </div>
           <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+        </div>
       </div>
     </div>
   </div>
-</div>  
+
+
+
+<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit Data Dinas</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?php echo base_url()?>hrd/medicalreimbust/edit"  method="post">
+        <?php	
+            $id = $this->input->get('id');
+            $cek_query = $this->medical->check_medical($id);
+
+            foreach ($cek_query->result_array() as $row)
+            {  
+              ?>
+              <div class="form-group">
+                <label>ID Karyawan</label>
+                <input type="number" name="id_karyawan" class="form-control" readonly placeholder="Nomor SPPD" value="<?php echo $row['id_karyawan'] ?>" required readonly>
+
+                <label>Nama Karyawan</label>
+                <input type="number" name="nama_karyawan" class="form-control" placeholder="Nama Karyawan" value="<?php echo $row['nama_karyawan'] ?>" required readonly>
+
+                <label>Claim ID</label>
+                <input type="date" name="klaim_id" class="form-control" placeholder="Claim ID" value="<?php echo $row['klaim_id'] ?>" required>
+
+                <label>Tanggal Pengajuan</label>
+                <input type="date" name="tanggal_pengajuan" class="form-control" placeholder="Keterangan" value="<?php echo $row['tanggal_pengajuan'] ?>" required>
+                
+                <label>Status Pengajuan</label>
+                <input type="text" name="status_pengajuan" class="form-control" placeholder="Status Pengajuan" value="<?php echo $row['status_pengajuan'] ?>" required readonly>
+
+                <label>Tanggal Disetujui</label>
+                <input type="text" name="tanggal_disetujui" class="form-control" placeholder="Tanggal Disetujui" value="<?php echo $row['tanggal_disetujui'] ?>" required readonly>
+
+                <label>Jumlah Diajukan</label>
+                <input type="text" name="jumlah_diajukan" class="form-control" placeholder="Jumlah Diajukan" value="<?php echo $row['jumlah_diajukan'] ?>" required>
+
+                <label>Tanggal DIsetujui</label>
+                <input type="text" name="tanggal_disetujui" class="form-control" placeholder="Uang Makan" value="<?php echo $row['tanggal_disetujui'] ?>" required readonly>
+
+
+              </div>
+              
+              <button type="submit" class="btn btn-primary">Edit</button>
+              <button type="reset" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <?php } ?>
+          </form>
+      </div>
+      <div class="modal-footer">
+
+      </div>
+    </div>
+  </div>
 </div>
+
+
+
+
 </div>
